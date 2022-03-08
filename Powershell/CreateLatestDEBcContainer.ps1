3167e02cc7c75ae225384a844edfd9dd2ab46958
Daniel Gorski <daniel.gorski@beyondit.gmbh>
2022-03-08 10:22:45 +0100

    added script to create local latest de bc container

---
+++ Powershell/CreateLatestDEBcContainer.ps1
@@ -0,0 +1,22 @@
+﻿Set-ExecutionPolicy Unrestricted
+Install-Module bccontainerhelper
+
+$RootPath = (Split-Path $PSScriptRoot)
+$containerName = 'de-latest'
+$credential = Get-Credential -Message 'Using UserPassword authentication. Please enter credentials for the container.'
+$auth = 'UserPassword'
+$artifactUrl = Get-BcArtifactUrl -type sandbox -country 'de' -select Latest
+$licenseFile = "C:\Temp\license.flf"
+
+New-BcContainer `
+    -accept_eula `
+    -containerName $containerName `
+    -credential $credential `
+    -auth $auth `
+    -artifactUrl $artifactUrl `
+    -licenseFile $licenseFile `
+    -isolation "hyperv" `
+    -memoryLimit 8G `
+    -includeAL `
+    -doNotExportObjectsToText `
+    -updateHosts
