4ee19b257a20c37fa1f5d8e4bdf36ab36fe80299
Daniel Gorski <daniel.gorski@beyondit.gmbh>
2022-04-06 22:43:51 +0200

    add local fonts to bc container

---
+++ Powershell/AddFontsToContainer.ps1
@@ -0,0 +1,5 @@
+﻿Import-Module bccontainerhelper
+$containerName  = "de-latest"
+
+Add-FontsToBcContainer -containername $containerName -path C:\Windows\Fonts
+Restart-BcContainer -containerName $containername
