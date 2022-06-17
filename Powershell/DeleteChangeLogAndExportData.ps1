8108c4197ac5f9d6fb72ec90f24836eacc8273f4
Daniel Gorski <daniel.gorski@beyondit.gmbh>
2022-06-17 08:38:34 +0200

    Delete and export Change Log older than x days

---
+++ Powershell/DeleteChangeLogAndExportData.ps1
@@ -0,0 +1,19 @@
+﻿Install-Module SqlServer -AllowClobber
+Import-Module SqlServer
+
+$db = "Demo Database BC (14-0)"
+$SQLServer = "localhost"
+$olderThanXdays = -365
+
+$companies = Invoke-Sqlcmd -ServerInstance $SQLServer -Database $db -Query 'SELECT Name from [dbo].[Company]'
+foreach ($company in $companies) {
+    $companyname = $company.Name
+    $query = 'SELECT * FROM dbo.['+$companyname+'$Change Log Entry] where [dbo].['+$companyname+'$Change Log Entry].[Date and Time] < DATEADD(day, '+$olderThanXdays+', SYSDATETIME())'
+    Invoke-Sqlcmd -ServerInstance $SQLServer -Database $db -Query $query | Export-Csv `
+        -NoTypeInformation `
+        -Path "C:\$companyname.csv" `
+        -Encoding UTF8
+
+    $deleteQuery = 'delete from [dbo].['+$companyname+'$Change Log Entry] where [dbo].['+$companyname+'$Change Log Entry].[Date and Time] < DATEADD(day, '+$olderThanXdays+', SYSDATETIME())'
+    Invoke-Sqlcmd -ServerInstance $SQLServer -Database $db -Query $deleteQuery
+}
