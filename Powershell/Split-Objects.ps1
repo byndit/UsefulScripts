c09544d5b0e140b9a594f71e23ed2030890bac02
Daniel Gorski <daniel.gorski@beyondit.gmbh>
2022-09-20 16:46:29 +0200

    added faster split

--- Powershell/Split-Objects.ps1
+++ Powershell/Split-Objects.ps1
@@ -40,3 +40,31 @@ Measure-Command -Expression {
         }
     }
 }
+
+#faster 
+$sourcepath = "D:\Source\"
+$targetpath = "D:\Target\"
+Remove-Item $targetpath*.txt -Recurse -Force
+Measure-Command -Expression {
+    Get-ChildItem $sourcepath  -Filter *.txt | Foreach-Object {
+        $filepath = $_.FullName
+        $lines = [IO.File]::OpenText($filepath)
+        while ( -not $lines.EndOfStream) {
+            $line = $lines.ReadLine()
+            if ($line -match "OBJECT (Table|Report|Codeunit|Page|Query|XMLport|Form|Dataport|MenuSuite) (\d+) (.*)") {
+                if ($newfilepath) {
+                    $Textstream = [System.IO.StreamWriter]::new($newfilepath, $true)
+                    $Textstream.Write($arr.ToString())
+                    $Textstream.close()
+                    $arr.Clear();
+                    $arr = [System.Text.StringBuilder]::new()
+                }
+                $newfilepath = (Join-Path -Path $targetpath -ChildPath ($Matches[1].ToLower() + '_' + ('{0:d10}' -f [int]$Matches[2]) + $_.Extension));
+            }
+            if ($line) {
+                [void]$arr.Append($line + "`r`n") 
+            }
+        }
+        $lines.close()
+    }
+}
